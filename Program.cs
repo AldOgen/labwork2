700fcbace59e01d6738823e3086b27753b796174
Егор Зубков <zegal2000@gmail.com>
2020-11-24 14:33:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace labwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1:");
+            V2DataOnGrid data_on_grid_1 = new V2DataOnGrid(0, "New V2 data on grid", new double[] { 0.01, 0.01 }, new int[] { 5, 5 });
+            Console.WriteLine(data_on_grid_1.ToLongString());
+            Console.WriteLine((((V2DataCollection)data_on_grid_1).ToLongString() + "\n"));
+            Console.WriteLine("2:");
+            V2MainCollection main_collection = new V2MainCollection();
+            main_collection.AddDefaults();
+            Console.WriteLine(main_collection);
+            Console.WriteLine("3:");
+            foreach (V2Data elem in main_collection) {
+                var mas = elem.NearAverage(2.0f);
+                foreach (var val in mas) {
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
